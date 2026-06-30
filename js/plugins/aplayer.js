81c666b410ad2e907a36cbb1a1a7c78268d1b1b3
shituku <zhuzikunckn@icloud.com>
2026-06-30 10:57:50 +0800

    Site updated: 2026-06-30 10:57:48

--- js/plugins/aplayer.js
+++ js/plugins/aplayer.js
@@ -2,6 +2,7 @@
   const audioList = [];
   const isFixed = theme.plugins.aplayer.type === "fixed";
   const isMini = theme.plugins.aplayer.type === "mini";
+  let hasLrc = false;
 
   for (const audio of theme.plugins.aplayer.audios) {
     const audioObj = {
@@ -9,9 +10,14 @@
       artist: audio.artist,
       url: audio.url,
       cover: audio.cover,
-      lrc: audio.lrc,
       theme: audio.theme,
     };
+
+    if (audio.lrc) {
+      audioObj.lrc = audio.lrc;
+      hasLrc = true;
+    }
+
     audioList.push(audioObj);
   }
 
@@ -25,9 +31,11 @@
     const player = new APlayer({
       container: document.getElementById("aplayer"),
       fixed: true,
-      lrcType: 3,
+      ...(hasLrc ? { lrcType: 3 } : {}),
       audio: audioList,
     });
-    document.querySelector(".aplayer-icon-lrc").click();
+    if (hasLrc) {
+      document.querySelector(".aplayer-icon-lrc").click();
+    }
   }
 })();
